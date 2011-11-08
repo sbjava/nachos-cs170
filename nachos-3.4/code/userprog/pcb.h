23f36b9ab7c69e651dfd7614d3e49bc0b59208a3
jmeeker516 <jmeeker516@d35791c3-fc34-914a-7b97-4d6e4c16ed82>
2011-11-08 04:47:18 +0000

    pcb and exception changed for Join sys call

--- nachos-3.4/code/userprog/pcb.h
+++ nachos-3.4/code/userprog/pcb.h
@@ -9,6 +9,11 @@
 #ifndef PCB_H_
 #define PCB_H_
 
+#define GOOD 0;
+#define BAD 1;
+#define RUNNING 2;
+#define BLOCKED 3;
+
 class Thread;
 
 class PCB {
@@ -16,6 +21,7 @@ public:
 	int pid;
 	int parentPid;
 	Thread *thread;
+	int status;
 	PCB();
 	~PCB();
 };
