fd507ed8e0a04ca3cfc5855c13792785fcf9d75a
jillschwalb <jillschwalb@hotmail.com>
2011-11-08 04:49:50 +0000

    join stuff for process manager

--- nachos-3.4/code/userprog/processmanager.h
+++ nachos-3.4/code/userprog/processmanager.h
@@ -15,10 +15,16 @@ public:
 	int getPID();		// return an unused PID
 	void clearPID(int pid); // clear out PID passed in
 	void insertProcess(PCB *pcb, int pid); //insert PCB into pcsb    
+ 	void join(int pid);	// keeps track of who is waiting on a condition variable for each PCB
+	int getStatus(int pid); // Get pid's process status
 
 private:
 	BitMap processes; 	// a BitMap for process manager methods
 	PCB** pcbs;		// an array of PCB* to store the PCBs
+	Condition **conditions;	// for join and exit
+	Lock **locks;		// for join and exit
+	int pcbStatus[MAX_PROCESESS];		// array of PCB statuses
+	int joinProcessNum[MAX_PROCESESS];	// array of join process numbers
 };
 
 #endif
