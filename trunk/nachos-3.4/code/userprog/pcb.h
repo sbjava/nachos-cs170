091132893108f2e675827431482bbfe43084d4b9
jmeeker516 <jmeeker516@d35791c3-fc34-914a-7b97-4d6e4c16ed82>
2011-11-09 01:29:38 +0000

    create sys call. useropenfile.h added. array of UserOpenFile's added to PCB

--- trunk/nachos-3.4/code/userprog/pcb.h
+++ trunk/nachos-3.4/code/userprog/pcb.h
@@ -9,6 +9,10 @@
 #ifndef PCB_H_
 #define PCB_H_
 
+#include "useropenfile.h"
+
+#define MAX_FILES 30
+
 #define PCB_GOOD 0;
 #define PCB_BAD 1;
 #define PCB_RUNNING 2;
@@ -22,6 +26,8 @@ public:
 	int parentPid;
 	Thread *thread;
 	int status;
+	UserOpenFile *fileArray[MAX_FILES]; 
+	UserOpenFile* getFile(int id);
 	PCB();
 	~PCB();
 };
