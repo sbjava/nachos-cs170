a61ccba44d10120e2fe610343dc889fb4d88d3c6
jillschwalb <jillschwalb@hotmail.com>
2011-11-09 01:36:03 +0000

    sysopenfile .h and .cc for real

---
+++ nachos-3.4/code/userprog/sysopenfile.cc
@@ -0,0 +1,118 @@
+// sysopenfile.cc
+//     Defines SysOpenFile class that contains a pointer to the file 
+//     system's OpenFile object and (char *)fileName for that file and 
+//     the number of user processes currently accessing it. Declare an 
+//     array of SysOpenFile objects for use by all system calls implemented.
+
+#include "sysopenfile.h"
+#include "myutilities.h"
+
+//----------------------------------------------------------------------
+// SysOpenFile::closeOne
+//      close a SysOpenFile and accessing processes
+//----------------------------------------------------------------------
+
+void SysOpenFile::closeOne() 
+{
+    if(userOpens <= 0)
+	return;
+    userOpens--;
+    if (userOpens == 0) 
+    {
+        delete fileName;
+        delete file;
+    }
+}
+//----------------------------------------------------------------------
+// SysOpenFile::close
+//      close all SysOpenFile and accessing processes
+//----------------------------------------------------------------------
+void SysOpenFile::close() 
+{
+    if (userOpens <= 0)
+	return;
+    userOpens = 0;
+    delete fileName;
+    delete file;
+}
+
+/*
+//----------------------------------------------------------------------
+// SysOpenFileManager::SysOpenFileManager
+//      Initialize a SysOpenFileManager with initialize a bitmap with
+//      "SOFILETABLE_SIZE"
+//----------------------------------------------------------------------
+SysOpenFileManager::SysOpenFileManager() : bitMap(SOFILETABLE_SIZE) 
+{
+}
+
+//----------------------------------------------------------------------
+// SysOpenFileManager::~SysOpenFileManager
+//      De-allocate a SysOpenFileManager
+//----------------------------------------------------------------------
+SysOpenFileManager::~SysOpenFileManager() 
+{
+}
+//----------------------------------------------------------------------
+// SysOpenFileManager::Add
+//      Add a SysOpenFile to the array of all SysOpenFile.
+//      Return the index of the added SysOpenFile
+//
+//      "soFile" is the SysOpenFile to be added.
+//----------------------------------------------------------------------
+int SysOpenFileManager::Add(SysOpenFile soFile) {
+        int i = bitMap.Find();
+        if (i < 0)
+                return i;//Fail to add, full
+        sysOpenFileTable[i] = soFile;
+        return i;
+}
+
+//----------------------------------------------------------------------
+// SysOpenFileManager::Get
+//      Get SysOpenFile by "fileName" and stored its index.
+//
+//      Return the added SysOpenFile
+//
+//      "fileName" is the name of the SysOpenFile to get.
+//      "index" is the index of the SysOpenFile to get in the array.
+//----------------------------------------------------------------------
+SysOpenFile* SysOpenFileManager::Get(char* fileName, int& index) {
+        for (int i = 2; i < SOFILETABLE_SIZE; i++){
+                if (bitMap.Test(i) && strcmp(fileName, sysOpenFileTable[i].fileName)) {
+                        index = i;
+                        return &sysOpenFileTable[i];
+                }
+        }
+        return NULL;
+}
+
+//----------------------------------------------------------------------
+// SysOpenFileManager::Get(index)
+//      Get SysOpenFile by "index" of the array
+//
+///      Return the got SysOpenFile if existed, or
+//       Return NULL
+//
+//      "index" is the number of index in the array to get.
+//----------------------------------------------------------------------
+SysOpenFile* SysOpenFileManager::Get(int index) {
+        if (bitMap.Test(index)) {
+                return sysOpenFileTable + index;
+        }
+        return NULL;
+}
+
+//----------------------------------------------------------------------
+// SysOpenFileManager::closeOne
+//      Close a SysOpenFile by "index" of the array
+//
+//      "index" is the number of index in the array to get.
+//----------------------------------------------------------------------
+void SysOpenFileManager::closeOne(int index) {
+        ASSERT(bitMap.Test(index));
+        sysOpenFileTable[index].closeOne();
+        if (sysOpenFileTable[index].userOpens == 0)
+                bitMap.Clear(index);
+}
+*/
---
+++ nachos-3.4/code/userprog/sysopenfile.h
@@ -0,0 +1,37 @@
+// SysOpenFile.h
+
+#ifndef SYSOPENFILE_H_
+#define SYSOPENFILE_H_
+#define SOFILETABLE_SIZE 100
+#include "filesys.h"
+#include "bitmap.h"
+
+// The following class defines a "SysOpenFile"
+
+class SysOpenFile{
+public:
+    OpenFile *file; 	// a pointer to the file system's OpenFile object
+    char * fileName;    // file name and the number of user processes accessing
+    int fileID;
+    int numUsersAccess;	// Number of user processes accessing
+    void closeOne();	//close a SysOpenFile and accessing processes
+    void close();   //close all SysOpenFile and accessing processes                                         
+};
+/*
+// The following class defines a "SysOpenFileManager"
+class SysOpenFileManager{
+public:
+    SysOpenFileManager();   // Initialize a SysOpenFileManager
+    ~SysOpenFileManager();  // De-allocate a SysOpenFileManager
+    int Add(SysOpenFile newSysOpenFile); //Add a SysOpenFile to the array
+    SysOpenFile* Get(char* fileName,int& index); // Get SysOpenFile by "fileName"
+    SysOpenFile* Get(int index);    // Get SysOpenFile by "index" of the array
+    void closeOne(int index);       // Close a SysOpenFile by "index"
+private:
+    SysOpenFile sysOpenFileTable[SOFILETABLE_SIZE]; // array for all SysOpenFile
+    BitMap bitMap;  // a BitMap for SysOpenFile manager methods
+
+};
+*/
+#endif /* SYSOPENFILE_H_ */
+
